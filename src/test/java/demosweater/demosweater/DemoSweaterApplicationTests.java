1a5f0eb23b3495788c48d1758082b39cdcd66850
Demotivirus <demotivirus@gmail.com>
2019-12-26 00:38:04 +0300

    Create repository

---
+++ src/test/java/demosweater/demosweater/DemoSweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package demosweater.demosweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSweaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
